dd6e515fdafea22e6c05195502b9027c38238ac0
edclements <ed.clements@gmail.com>
2017-02-20 17:51:19 +0000

    Version v2.2.0

--- bookingbug-angular-settings.js
+++ bookingbug-angular-settings.js
@@ -1,186 +1,174 @@
-(function() {
-  'use strict';
-  angular.module('BBAdminSettings', ['BB', 'BBAdmin.Services', 'BBAdmin.Filters', 'BBAdmin.Controllers', 'trNgGrid']);
+'use strict';
 
-  angular.module('BBAdminSettingsMockE2E', ['BBAdminSettings', 'BBAdminMockE2E']);
+angular.module('BBAdminSettings', ['BB', 'BBAdmin.Services', 'BBAdmin.Filters', 'BBAdmin.Controllers', 'trNgGrid']);
 
-}).call(this);
+angular.module('BBAdminSettingsMockE2E', ['BBAdminSettings', 'BBAdminMockE2E']);
+'use strict';
 
-(function() {
-  'use strict';
-  angular.module('BBAdminSettings').config(function($logProvider) {
+angular.module('BBAdminSettings').config(function ($logProvider) {
     'ngInject';
+
     $logProvider.debugEnabled(true);
-  });
+});
+'use strict';
 
-}).call(this);
+angular.module('BBAdminSettings').directive('adminTable', function ($log, ModalForm, BBModel) {
 
-(function() {
-  'use strict';
-  angular.module('BBAdminSettings').directive('adminTable', function($log, ModalForm, BBModel) {
-    var controller, link;
-    controller = function($scope) {
-      $scope.getAdministrators = function() {
-        var params;
-        params = {
-          company: $scope.company
+    var controller = function controller($scope) {
+
+        $scope.getAdministrators = function () {
+            var params = { company: $scope.company };
+            return BBModel.Admin.Administrator.$query(params).then(function (administrators) {
+                $scope.admin_models = administrators;
+                return $scope.administrators = _.map(administrators, function (administrator) {
+                    return _.pick(administrator, 'id', 'name', 'email', 'role');
+                });
+            });
         };
-        return BBModel.Admin.Administrator.$query(params).then(function(administrators) {
-          $scope.admin_models = administrators;
-          return $scope.administrators = _.map(administrators, function(administrator) {
-            return _.pick(administrator, 'id', 'name', 'email', 'role');
-          });
-        });
-      };
-      $scope.newAdministrator = function() {
-        return ModalForm["new"]({
-          company: $scope.company,
-          title: 'New Administrator',
-          new_rel: 'new_administrator',
-          post_rel: 'administrators',
-          success: function(administrator) {
-            return $scope.administrators.push(administrator);
-          }
-        });
-      };
-      return $scope.edit = function(id) {
-        var admin;
-        admin = _.find($scope.admin_models, function(p) {
-          return p.id === id;
-        });
-        return ModalForm.edit({
-          model: admin,
-          title: 'Edit Administrator'
-        });
-      };
+
+        $scope.newAdministrator = function () {
+            return ModalForm.new({
+                company: $scope.company,
+                title: 'New Administrator',
+                new_rel: 'new_administrator',
+                post_rel: 'administrators',
+                success: function success(administrator) {
+                    return $scope.administrators.push(administrator);
+                }
+            });
+        };
+
+        return $scope.edit = function (id) {
+            var admin = _.find($scope.admin_models, function (p) {
+                return p.id === id;
+            });
+            return ModalForm.edit({
+                model: admin,
+                title: 'Edit Administrator'
+            });
+        };
     };
-    link = function(scope, element, attrs) {
-      if (scope.company) {
-        return scope.getAdministrators();
-      } else {
-        return BBModel.Admin.Company.$query(attrs).then(function(company) {
-          scope.company = company;
-          return scope.getAdministrators();
-        });
-      }
-    };
-    return {
-      controller: controller,
-      link: link,
-      templateUrl: 'admin-table/admin_table_main.html'
-    };
-  });
 
-}).call(this);
-
-(function() {
-  'use strict';
-  var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
-    hasProp = {}.hasOwnProperty;
-
-  angular.module('BB.Models').factory("Admin.AdministratorModel", function($q, AdminAdministratorService, BBModel, BaseModel) {
-    var Admin_Administrator;
-    return Admin_Administrator = (function(superClass) {
-      extend(Admin_Administrator, superClass);
-
-      function Admin_Administrator(data) {
-        Admin_Administrator.__super__.constructor.call(this, data);
-      }
-
-      Admin_Administrator.$query = function(params) {
-        return AdminAdministratorService.query(params);
-      };
-
-      return Admin_Administrator;
-
-    })(BaseModel);
-  });
-
-}).call(this);
-
-(function() {
-  'use strict';
-  var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
-    hasProp = {}.hasOwnProperty;
-
-  angular.module('BB.Models').factory("Admin.UserModel", function($q, BBModel, BaseModel) {
-    var Admin_User;
-    return Admin_User = (function(superClass) {
-      extend(Admin_User, superClass);
-
-      function Admin_User(data) {
-        Admin_User.__super__.constructor.call(this, data);
-        this.companies = [];
-        if (data) {
-          if (this.$has('companies')) {
-            this.$get('companies').then((function(_this) {
-              return function(comps) {
-                return _this.companies = comps;
-              };
-            })(this));
-          }
+    var link = function link(scope, element, attrs) {
+        if (scope.company) {
+            return scope.getAdministrators();
+        } else {
+            return BBModel.Admin.Company.$query(attrs).then(function (company) {
+                scope.company = company;
+                return scope.getAdministrators();
+            });
         }
-      }
-
-      return Admin_User;
-
-    })(BaseModel);
-  });
-
-}).call(this);
-
-(function() {
-  'use strict';
-  angular.module('BBAdmin.Services').factory('AdminAdministratorService', function($q, BBModel) {
-    return {
-      query: function(params) {
-        var company, defer;
-        company = params.company;
-        defer = $q.defer();
-        company.$get('administrators').then(function(collection) {
-          return collection.$get('administrators').then(function(administrators) {
-            var a, models;
-            models = (function() {
-              var i, len, results;
-              results = [];
-              for (i = 0, len = administrators.length; i < len; i++) {
-                a = administrators[i];
-                results.push(new BBModel.Admin.Administrator(a));
-              }
-              return results;
-            })();
-            return defer.resolve(models);
-          }, function(err) {
-            return defer.reject(err);
-          });
-        }, function(err) {
-          return defer.reject(err);
-        });
-        return defer.promise;
-      }
     };
-  });
 
-}).call(this);
+    return {
+        controller: controller,
+        link: link,
+        templateUrl: 'admin-table/admin_table_main.html'
+    };
+});
+"use strict";
 
-(function() {
-  "use strict";
-  angular.module("BBAdminSettings").config(function($translateProvider) {
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+angular.module('BB.Models').factory("Admin.AdministratorModel", function ($q, AdminAdministratorService, BBModel, BaseModel) {
+    return function (_BaseModel) {
+        _inherits(Admin_Administrator, _BaseModel);
+
+        function Admin_Administrator(data) {
+            _classCallCheck(this, Admin_Administrator);
+
+            return _possibleConstructorReturn(this, (Admin_Administrator.__proto__ || Object.getPrototypeOf(Admin_Administrator)).call(this, data));
+        }
+
+        _createClass(Admin_Administrator, null, [{
+            key: "$query",
+            value: function $query(params) {
+                return AdminAdministratorService.query(params);
+            }
+        }]);
+
+        return Admin_Administrator;
+    }(BaseModel);
+});
+'use strict';
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+angular.module('BB.Models').factory("Admin.UserModel", function ($q, BBModel, BaseModel) {
+    return function (_BaseModel) {
+        _inherits(Admin_User, _BaseModel);
+
+        function Admin_User(data) {
+            _classCallCheck(this, Admin_User);
+
+            var _this = _possibleConstructorReturn(this, (Admin_User.__proto__ || Object.getPrototypeOf(Admin_User)).call(this, data));
+
+            _this.companies = [];
+            if (data) {
+                if (_this.$has('companies')) {
+                    _this.$get('companies').then(function (comps) {
+                        return _this.companies = comps;
+                    });
+                }
+            }
+            return _this;
+        }
+
+        return Admin_User;
+    }(BaseModel);
+});
+'use strict';
+
+angular.module('BBAdmin.Services').factory('AdminAdministratorService', function ($q, BBModel) {
+
+    return {
+        query: function query(params) {
+            var company = params.company;
+
+            var defer = $q.defer();
+            company.$get('administrators').then(function (collection) {
+                return collection.$get('administrators').then(function (administrators) {
+                    var models = Array.from(administrators).map(function (a) {
+                        return new BBModel.Admin.Administrator(a);
+                    });
+                    return defer.resolve(models);
+                }, function (err) {
+                    return defer.reject(err);
+                });
+            }, function (err) {
+                return defer.reject(err);
+            });
+            return defer.promise;
+        }
+    };
+});
+"use strict";
+
+angular.module("BBAdminSettings").config(function ($translateProvider) {
     "ngInject";
-    var translations;
-    translations = {
-      SETTINGS: {
-        ADMIN_TABLE: {
-          NEW_ADMINISTRATOR: "New Administrator",
-          EDIT: "@:COMMON.BTN.EDIT"
-        },
-        ADMIN_FORM: {
-          OK_BTN: "@:COMMON.BTN.OK",
-          CANCEL_BTN: "@:COMMON.BTN.CANCEL"
-        }
-      }
-    };
-    $translateProvider.translations("en", translations);
-  });
 
-}).call(this);
+    var translations = {
+        SETTINGS: {
+            ADMIN_TABLE: {
+                NEW_ADMINISTRATOR: "New Administrator",
+                EDIT: "@:COMMON.BTN.EDIT"
+            },
+            ADMIN_FORM: {
+                OK_BTN: "@:COMMON.BTN.OK",
+                CANCEL_BTN: "@:COMMON.BTN.CANCEL"
+            }
+        }
+    };
+
+    $translateProvider.translations("en", translations);
+});
